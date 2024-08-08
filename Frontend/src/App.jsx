145692b0f747cb557fde31d3031f7968b5fbf1e9
Karunesh04 <karuneshtripathi2004@gmail.com>
2024-08-08 10:44:46 +0530

    added footer component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello Karunesh!</h1>
+      {/* <Footer /> */}
     </>
   );
 }
