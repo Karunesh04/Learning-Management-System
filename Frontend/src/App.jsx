8d97352c92e0e3ce7ac2b64981c58080469733b9
Karunesh04 <karuneshtripathi2004@gmail.com>
2024-08-10 02:40:19 +0530

    added about us page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 
+import AboutUS from "./pages/AboutUs";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<AboutUS />} />
     </Routes>
   );
 }
---
+++ Frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,119 @@
+import aboutMainPage from "../assets/Images/aboutMainImage.png";
+import apj from "../assets/Images/apj.png";
+import einstein from "../assets/Images/einstein.png";
+import nelsonMandela from "../assets/Images/nelsonMandela.png";
+import steveJobs from "../assets/Images/steveJobs.png";
+import { HomeLayout } from "../layouts/homeLayout";
+
+export default function AboutUs() {
+  return (
+    <HomeLayout>
+      <div className="flex flex-col text-white pl-20 pt-20">
+        <div className="flex items-center gap-5 mx-10">
+          <section className="w-1/2 space-y-10">
+            <h1 className="text-5xl text-yellow-500 font-semibold">
+              Affordable and quality education
+            </h1>
+            <p className="text-xl text-gray-200">
+              Our goal is to provide the affordable and quality education to the
+              world. We are providing the platform for the aspiring teschers and
+              students to share their skills, creativity and knowledge to each
+              other to empower and contribute in the growth and wellness of
+              mankind.
+            </p>
+          </section>
+          <section className="w-1/2">
+            <img
+              src={aboutMainPage}
+              className="drop-shadow-2xl"
+              alt="about main page"
+              id="test1"
+            />
+          </section>
+        </div>
+        <div className="carousel w-1/2 my-10 m-auto">
+          <div id="slide1" className="carousel-item relative w-full">
+            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+              <img
+                src={apj}
+                className="w-40 rounded-full border-2 border-gray-400"
+              />
+              <p className="text-xl text-gray-200">
+              Teaching is a very noble profession that shapes the character, caliber, and future of an individual.
+              </p>
+              <h3 className="text-2xl font-semibold">Dr. APJ Abdul Kalam</h3>
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide4" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide2" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full">
+          <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+              <img
+                src={steveJobs}
+                className="w-40 rounded-full border-2 border-gray-400"
+              />
+              <p className="text-xl text-gray-200">
+              We dont get a chance to do that many things, and every one should be really excellent.
+              </p>
+              <h3 className="text-2xl font-semibold">Steve Jobs</h3>
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide1" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide3" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+          <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+              <img
+                src={nelsonMandela}
+                className="w-40 rounded-full border-2 border-gray-400"
+              />
+              <p className="text-xl text-gray-200">
+              Education is the most powerful tool you can use to change the world.
+              </p>
+              <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide2" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide4" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+          <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+              <img
+                src={einstein}
+                className="w-40 rounded-full border-2 border-gray-400"
+              />
+              <p className="text-xl text-gray-200">
+              Education is not the learning of facts, but the training of the mind to think.
+              </p>
+              <h3 className="text-2xl font-semibold">Albert Einstein</h3>
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href="#slide3" className="btn btn-circle">
+                  ❮
+                </a>
+                <a href="#slide1" className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </HomeLayout>
+  );
+}
