4c44770f4a7ac6822fcbad86da39a44ce8292fac
Karunesh04 <karuneshtripathi2004@gmail.com>
2024-08-12 00:24:46 +0530

    added signup page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 
-import AboutUS from "./pages/AboutUs";
+import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
 import NotFound  from "./pages/NotFound";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutUS />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
